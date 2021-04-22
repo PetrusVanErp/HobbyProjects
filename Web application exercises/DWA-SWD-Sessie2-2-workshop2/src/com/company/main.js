25b5525513eaf5005ec33c6a6b79b574e8c3d25a
Pepijn van Erp <pepijnvanerp98@outlook.com>
2021-04-22 14:04:24 +0200

    changed location of the compiler

---
+++ Web application exercises/DWA-SWD-Sessie2-2-workshop2/src/com/company/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const readline = require('readline');
+const game = require('./game');
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const COMMAND_ERROR = Symbol();
+
+rl.setPrompt('action?> ');
+rl.prompt();
+
+rl.on('line', (line) => {
+    const [command, argument] = line.trim().split(' ');
+    execute(command, argument).then(data => {
+        console.log(data)
+    });
+
+}).on('close', function () {
+    //DEFAULT ^c
+    console.log('Leaving the game');
+    process.exit(0);
+});
+
+async function execute(command, argument) {
+    let response;
+    switch (command) {
+        case 'where':
+        case 'w':
+            const locationInformation = await game.getLocationInformation()
+            response = `you are in ${locationInformation.description}`;
+            response += '\nand you can go to these location(s): '
+
+            response += locationInformation.exits.reduce((allExits, exit)  => {
+                return allExits + `\n- ${exit}`;
+            }, '');
+
+            return response;
+
+        case 'goto':
+        case 'g':
+            if (argument === null || argument === undefined) {
+                let err = new Error(`The input '${command}' needs an argument`)
+                err.code = COMMAND_ERROR;
+                return Promise.reject(err)
+            }
+            return new Promise((resolve, reject) => {
+                game.goToLocation(argument).then(data => {
+                    resolve(data)
+                }).catch(err => {
+                    reject(err)
+                })
+            })
+        default:
+            let err = new Error(`The input: '${command}' is not defined`)
+            err.code = COMMAND_ERROR;
+            return Promise.reject(err);
+    }
+}
